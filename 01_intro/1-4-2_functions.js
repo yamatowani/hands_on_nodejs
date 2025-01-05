d292c5589836c0345461e5e5f223f74789560572
yamatowani <festajaune.18637@icloud.com>
2025-01-05 16:05:20 +0900

    add: objects.js

--- 01_intro/1-4-2_functions.js
+++ 01_intro/1-4-2_functions.js
@@ -5,5 +5,17 @@ const add2 = function(a, b) { return a + b}
 const add3 = function addFn(a, b) {return a + b}
 
 console.log(add1.name)
-console.log(add2.name)
-console.log(add3.name)
+console.log(add2.name) 
+console.log(add3.name) 
+
+const add4 = (a, b) => {
+  return  a + b
+}
+
+const add5 = (a, b) => a + b
+
+const addOne = a => a + 1
+
+
+
+
---
+++ 01_intro/1-4-3_objects.js
@@ -0,0 +1,24 @@
+const obj1 = { propA: 1, propB: 2 }
+
+obj1.propA
+
+obj1['propA']
+
+obj1.propC = 3
+
+delete obj1.propC
+
+const obj2 = { ...obj1, propC: 3} ///スプレッド構文でexucute
+
+const { propA, ...obj3 } = obj2
+
+const obj4 = { propB: 'b', propD: 'd' }
+
+Object.keys(obj2) // ['propA','propB', 'propC']
+
+Object.values(obj2) //  [ 1, 2, 3]
+
+Object.entries(obj2) //[ [ 'propA', 1], [ 'propB', 2], [ 'propC', 3]]
+
+
+
